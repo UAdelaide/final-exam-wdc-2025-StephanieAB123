ab579cfbb0510028c95cb2a0cfe0c47c8486bd34
WDC Student (missing default git user name) <a1877567@adelaide.edu.au>
2025-06-20 11:47:38 +0000

    [automatic commit] 2025-06-20 11:47:37 UTC 122.108.115.248

--- part1/app.js
+++ part1/app.js
@@ -166,7 +166,10 @@ app.get('/api/walkrequests/open', async (req, res) => {
             SELECT
                 wr.request_id,
                 d.name AS dog_name,
-                `);
+                wr.requested_time,
+                wr.duration_minutes,
+                wr.location,
+                u.username AS owner_username`);
         res.json(rows);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch walk requests' });
