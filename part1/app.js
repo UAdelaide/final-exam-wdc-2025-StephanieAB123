d87dfa6a73a156d3f61f311cdef843813ac27467
WDC Student (missing default git user name) <a1877567@adelaide.edu.au>
2025-06-20 11:34:47 +0000

    [automatic commit] 2025-06-20 11:34:46 UTC 122.108.115.248

--- part1/app.js
+++ part1/app.js
@@ -105,10 +105,12 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+        INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'), 
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('davidjones', 'david@example.com', 'hashed101', 'walker'),
+        ('emulace', 'emu@example.com', 'hashed112', 'owner');
       `);
     }
   } catch (err) {
