ce4511872f909bf0bc3a3b9e3f1493522054a88e
WDC Student (missing default git user name) <a1877567@adelaide.edu.au>
2025-06-20 11:35:55 +0000

    [automatic commit] 2025-06-20 11:35:55 UTC 122.108.115.248

--- part1/app.js
+++ part1/app.js
@@ -118,21 +118,21 @@ let db;
   }
 })();
 
-const [users] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-    if (users[0].count === 0) {
-      await db.execute(`
-        INSERT INTO Users (username, email, password_hash, role) VALUES
-        ('alice123', 'alice@example.com', 'hashed123', 'owner'), 
-        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        ('davidjones', 'david@example.com', 'hashed101', 'walker'),
-        ('emulace', 'emu@example.com', 'hashed112', 'owner');
-      `);
+    const [dogs] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
+        if (users[0].count === 0) {
+        await db.execute(`
+            INSERT INTO Users (username, email, password_hash, role) VALUES
+            ('alice123', 'alice@example.com', 'hashed123', 'owner'), 
+            ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+            ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+            ('davidjones', 'david@example.com', 'hashed101', 'walker'),
+            ('emulace', 'emu@example.com', 'hashed112', 'owner');
+        `);
+        }
+    } catch (err) {
+        console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
     }
-  } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
-})();
+    })();
 
 
 
