4b43c0cf53a9bcb4c4b9036d2649987562a8dfba
WDC Student (missing default git user name) <a1877567@adelaide.edu.au>
2025-06-20 11:44:47 +0000

    [automatic commit] 2025-06-20 11:44:47 UTC 122.108.115.248

--- part1/app.js
+++ part1/app.js
@@ -153,7 +153,7 @@ app.get('/api/dogs', async (req, res) => {
                 u.username AS owner_username
             FROM Dogs d
             JOIN Users u ON d.owner_id = u.user_id
-            `);
+        `);
         res.json(rows);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch dogs' });
