bd2b3f601b5177a64fce5aa2f29a716805049465
WDC Student (missing default git user name) <a1877567@adelaide.edu.au>
2025-06-21 03:01:47 +0000

    [automatic commit] 2025-06-21 03:01:47 UTC 122.108.115.248

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,7 +35,9 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-router.get('/mydogs')
+router.get('/mydogs', async (req, res) => {
+  const 
+})
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
